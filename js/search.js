ce1d60762db70ac0bc16d35cf791dbc2cf2815d0
InnaHTML <mavka0208@gmail.com>
2025-04-03 15:06:34 +0300

    add

--- js/search.js
+++ js/search.js
@@ -42,8 +42,8 @@ const search = () => {
 
           localStorage.setItem("goods", JSON.stringify(array));
         
-          if (window.location.pathname !== "/goods.html") {
-            window.location.href = "/goods.html";
+          if (window.location.pathname !== "../goods.html") {
+            window.location.href = "../goods.html";
           } else {
             renderGoods(array);
           }
